4b06598e3bf8da8283eba73f9e7c3c315e7e4cbc
mayank dixit <44468368+mayank0802@users.noreply.github.com>
2020-06-08 14:47:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    target = os.environ.get('TARGET', 'aditi')
+    target = os.environ.get('TARGET', 'mayank')
     return 'Hello {}!\n'.format(target)
 
 if __name__ == "__main__":
